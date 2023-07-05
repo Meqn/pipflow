52e6a52de89bae44180bbe6da099bdf597c1caf2
Mervin <mengqing723@gmail.com>
2023-07-05 22:17:36 +0800

    feat: add user-task

---
+++ packages/core/build/user.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp')
+const { pipeline } = require('../base/utils')
+
+module.exports = function userTask(options = {}, done) {
+  const {
+    compiler,
+    plugins
+  } = options
+
+  // 1. 自定义处理函数
+  if (typeof compiler === 'function') {
+    return compiler({
+      gulp
+    }, done)
+  }
+
+  // 2. 自定义处理流程
+  if (Array.isArray(plugins) && plugins.length > 0) {
+    return pipeline(plugins).on('end', done)
+  }
+
+  return done()
+}
