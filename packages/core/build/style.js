c86c43fd60238350f5e28305850dd6b4eb50115e
Mervin <mengqing723@gmail.com>
2023-07-06 21:54:22 +0800

    feat: 增加css-preprocessor

--- packages/core/build/style.js
+++ packages/core/build/style.js
@@ -1,6 +1,14 @@
 const gulp = require('gulp')
+const _ = require('lodash')
 const sourcemaps = require('gulp-sourcemaps')
 const filter = require('gulp-filter')
+const replace = require('gulp-replace')
+const sass = require('gulp-sass')(require('sass'))
+const less = require('gulp-less')
+const stylus = require('gulp-stylus')
+const postcss = require('gulp-postcss')
+const postcssEnv = require('postcss-preset-env')
+const cssnano = require('cssnano')
 
 const { pipeline } = require('../base/utils')
 const { createSrcOptions, outputFiles, plumber, putProcesses } = require('./comm')
@@ -10,6 +18,9 @@ module.exports = function styleTask(options = {}, done) {
     input,
     dest,
     fileHash,
+    compiler,
+    alias,
+    minify: isMinify,
     sourcemap: hasSourcemap,
   } = options
 
@@ -31,6 +42,28 @@ module.exports = function styleTask(options = {}, done) {
 
   // 1. 自定义处理流程
   putProcesses(processes, options.plugins)
+
+  // 2. replace 替换别名
+  if (_.isPlainObject(alias)) {
+    for (const key in alias) {
+      processes.push(replace(key, alias[key]))
+    }
+  }
+
+  // 3. CSS预处理器
+  if (compiler === 'sass' || compiler === 'scss') {
+    processes.push(sass().on('error', sass.logError))
+  } else if (compiler === 'less') {
+    processes.push(less())
+  } else if (compiler === 'stylus') {
+    processes.push(stylus())
+  }
+
+  // 4. postcss
+  const postcssPlugins = []
+  postcssPlugins.push(postcssEnv())
+  isMinify && postcssPlugins.push(cssnano())
+  processes.push(postcss(postcssPlugins))
 
   // 9. 文件指纹处理 & sourcemaps & 输出文件
   outputFiles(processes, {
