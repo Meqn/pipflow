a5bef9cbe14ffd7951cf1b213bd474209cc6581c
Mervin <mengqing723@gmail.com>
2023-07-10 18:23:25 +0800

    feat: add html-taks

---
+++ packages/core/build/html.js
@@ -0,0 +1,133 @@
+const gulp = require('gulp')
+const path = require('path')
+const { readFileSync } = require('fs')
+const _ = require('lodash')
+const replace = require('gulp-replace')
+const rename = require('gulp-rename')
+const htmlMinifier = require('../plugins/htmlMinifier')
+const revRewrite = require('gulp-rev-rewrite')
+const ejsTemplate = require('gulp-ejs')
+const pugTemplate = require('gulp-pug')
+const hbTemplate = require('gulp-hb')
+const njkTemplate = require('gulp-nunjucks')
+const artTemplate = require('gulp-art-tpl')
+
+const { pipeline } = require('../base/utils')
+const { createSrcOptions, outputFiles, plumber, putProcesses } = require('./comm')
+const { envInject } = require('../base/config')
+
+/**
+ * html 模板引擎
+ * @param {string} compiler 模板引擎name
+ * @param {object} compilerOptions 配置项
+ * @returns 
+ */
+function templater(compiler, compilerOptions = {}) {
+  const { data = {} } = compilerOptions
+  const templaterMap = {
+    ejs() {
+      // ejs(data, options)
+      return ejsTemplate(data, compilerOptions)
+    },
+    pug() {
+      // pug([opts])
+      return pugTemplate(compilerOptions)
+    },
+    artTemplate() {
+      // template(data, options)
+      return artTemplate(data, compilerOptions)
+    },
+    handlebars() {
+      // hb([options])
+      return hbTemplate(compilerOptions)
+    },
+    nunjucks() {
+      // nunjucks.compile(data?, options?)
+      return njkTemplate(data, compilerOptions)
+    }
+  }
+  return templaterMap[compiler]?.()
+}
+
+module.exports = function htmlTask(options = {}, done) {
+  const {
+    input,
+    dest,
+    compiler,
+    compilerOptions,
+    alias,
+    fileHash,
+    minify: isMinify,
+  } = options
+  
+  if (!input) {
+    throw new Error('input is required')
+  }
+  
+  let manifest
+  if (fileHash) {
+    manifest = readFileSync(path.resolve(dest, 'rev-manifest.json'), 'utf-8')
+  }
+  
+  const processes = []
+  const srcOptions = createSrcOptions(options)
+
+  // 1. plumber错误处理
+  processes.push(plumber.handler())
+
+  // 2. 环境变量处理
+  processes.push(envInject({ isVar: false }))
+
+  // 3. replace 替换别名
+  if (_.isPlainObject(alias)) {
+    for (const key in alias) {
+      console.log('alias : ', key, alias[key])
+      processes.push(replace(key, alias[key]))
+    }
+  }
+
+  // 4. 自定义处理流程
+  putProcesses(processes, options.plugins)
+
+  // 5. 模板处理
+  if (compiler) {
+    const rendered = templater(compiler, compilerOptions)
+    if (rendered) {
+      processes.push(rendered)
+    }
+  }
+  
+  // 6. 文件指纹处理
+  if (manifest) {
+    processes.push(revRewrite({ manifest }))
+  }
+  
+  // 7. 重命名
+  processes.push(rename({ extname: '.html' }))
+
+  // 8. 压缩处理
+  if (isMinify) {
+    processes.push(htmlMinifier({
+      collapseWhitespace: true, //移除多余空白
+      removeComments: true, //移除注释
+      // removeRedundantAttributes: true, //移除默认值的属性
+      removeEmptyAttributes: true, //移除空的属性
+      // removeAttributeQuotes: true, //移除属性值周围的引号，仅在可能的情况下使用
+      collapseBooleanAttributes: true, //当属性值为布尔类型时，移除属性值，仅保留属性名称
+      minifyJS: true, //使用terser来压缩内联JavaScript代码
+      minifyCSS: true //使用clean-css压缩内联CSS代码
+    }))
+  }
+
+  // 9. 文件指纹处理 & sourcemaps & 输出文件
+  outputFiles(processes, {
+    dest,
+    fileHash: false,
+    sourcemap: false,
+  })
+
+  return pipeline(
+    gulp.src(input, srcOptions),
+    processes
+  ).on('end', done)
+}
