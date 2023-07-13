9473d13d00b860b3e3deb9500d329ed9c0cd9fae
Mervin <mengqing723@gmail.com>
2023-07-13 17:20:52 +0800

    feat: gerenate gulp tasks

--- packages/main/libs/utils.js
+++ packages/main/libs/utils.js
@@ -1,26 +1,62 @@
-const path = require('path')
-const importFresh = require('import-fresh')
-// const { cosmiconfig } = require('cosmiconfig')
-
 /**
- * 获取用户自定义配置
- * 由于配置文件简单, 故暂不使用 `cosmiconfig`
- * @param {string|object} filePath 配置文件, 比如 `pipflow.config`, 可读取 `js|json`文件
- * @param {string} filePath.cwd 当前运行目录
- * @param {string} filePath.path 文件路径
+ * 路径下的所有文件
+ * @param {string} path 目录
+ * @param {number} level 层级
  * @returns 
  */
-function getUserConfig(filePath) {
-  let fileContent
-  if (typeof filePath === 'string') {
-    fileContent = importFresh(path.resolve(filePath))
+function globFiles(path, level = 2) {
+  if (!path) return ''
+  const regEx = /\/\*{1,2}$/
+  if (regEx.test(path)) {
+    return path
   }
-  if (typeof filePath === 'object') {
-    fileContent = importFresh(path.resolve(filePath.cwd, filePath.path))
+
+  const arr = path.split('/').filter(v => v)
+  arr.push(''.padEnd(level, '*'))
+  return arr.join('/')
+}
+
+/**
+ * 获取命令行 serve配置
+ * @param {object} options 选项
+ * @param {string} options.port 端口
+ * @param {boolean} options.open 是否打开
+ * @param {boolean} options.cors 是否允许跨域
+ * @param {string} options.dir 服务基础目录
+ * @returns 
+ */
+function getCliServeArgs({ port, open, cors, dir }) {
+  const args = {}
+  port && (args.port = port)
+  open && (args.open = open)
+  cors && (args.cors = cors)
+  dir && (args.server = { baseDir: dir })
+
+  return args
+}
+
+/**
+ * 获取所有输入文件列表
+ * @param {string|array|object} input 输入文件
+ * @returns 
+ */
+function getInputList(input) {
+  if (typeof input === 'string') {
+    return [].concat(input)
   }
-  return typeof fileContent === 'function' ? fileContent() : fileContent
+  if (Array.isArray(input)) {
+    return input
+  }
+  if (typeof input === 'object') {
+    const list = Object.keys(input).reduce((arr, key) => {
+      return arr.concat(input[key])
+    }, [])
+    return Array.from(new Set(list))
+  }
 }
 
 module.exports = {
-  getUserConfig
+  globFiles,
+  getCliServeArgs，
+  getInputList
 }
---
+++ packages/main/tasks.js
@@ -0,0 +1,180 @@
+const { task, watch, series, parallel } = require('gulp')
+const _ = require('lodash')
+
+const {
+  htmlTask,
+  scriptTask,
+  styleTask,
+  staticTask,
+  copyTask,
+  archiveTask,
+  removeTask,
+  userTask,
+  createServeTask
+} = require('@pipflow/core')
+const { getConfig } = require('@pipflow/utils')
+const { globFiles, getCliServeArgs, getInputList } = require('./libs/utils')
+
+const nodeEnv = process.env.NODE_ENV
+if (!nodeEnv) {
+  process.env.NODE_ENV = ['development', 'production'].includes(process.env.PP_MODE) ? process.env.PP_MODE : 'development'
+}
+
+// 命令行参数
+const args = minimist(process.argv.slice(3))
+
+//== 自定义配置 ==============================================
+const CC = getConfig(args.config || 'pipflow.config')
+const { outDir, archive } = CC.build
+const publicFiles = globFiles(CC.publicDir, 2) // public 目录文件
+
+//== 用户任务 ==============================================
+const taskTypes = {} //所有任务类型对象, `{ type: [{name}] }`
+const taskMap = {
+  html(options, done) {
+    return htmlTask(options, done)
+  },
+  script(options, done) {
+    return scriptTask(options, done)
+  },
+  style(options, done) {
+    return styleTask(options, done)
+  },
+  static(options, done) {
+    return staticTask(options, done)
+  },
+  copy(options, done) {
+    return copyTask(options, done)
+  },
+  remove(options, done) {
+    return removeTask(options, done)
+  },
+  archive(options, done) {
+    return archiveTask(options, done)
+  },
+  server(options, done) {
+    const serveTask = createServeTask(options.name)
+    return serveTask(options, done)
+  },
+  user(options, done) {
+    return userTask(options, done)
+  }
+}
+if (CC.tasks?.length > 0) {
+  for (const item of CC.tasks) {
+    // 任务类型数据
+    if (!taskTypes[item.type]) {
+      taskTypes[item.type] = []
+    }
+    taskTypes[item.type].push({
+      name: item.name
+    })
+    
+    // 🍱 所有自定义任务
+    task(item.name, done => {
+      taskMap[item.type]?.(item, done)
+    })
+  }
+}
+
+/**
+ * 🍔 创建 public 任务
+ */
+if (publicFiles) {
+  task('copy:public', done => {
+    copyTask({
+      input: publicFiles,
+      dest: outDir
+    }, done)
+  })
+}
+
+/**
+ * 📦 创建 archive:dest 压缩包
+ */
+task('archive:dest', done => {
+  archiveTask({
+    input: outDir,
+    filename: typeof archive === 'string' ? archive : `dest-${Date.now()}`
+  }, done)
+})
+
+//== 内置任务 ==============================================
+/**
+ * 👻 本地开发服务
+ */
+const devServerTask = createServeTask('pipFlowDev')
+const devServerReload = devServerTask.reload
+task('devServer', done => {
+  const cliServe = getCliServeArgs(args)
+  devServerTask(_.merge({}, CC.server, cliServe), done)
+})
+
+/**
+ * 👻 本地预览服务
+ */
+const viewServerTask = createServeTask('pipFlowView')
+task('preview', done => {
+  const cliServe = getCliServeArgs(args)
+  viewServerTask(_.merge({}, CC.server, cliServe), done)
+})
+
+/**
+ * 🗑 移除 dest 目录
+ */
+task('del:dest', done => {
+  removeTask(outDir, done)
+})
+
+/**
+ * 👀 监听任务
+ */
+task('watch', done => {
+  if (!CC.tasks.length) return done()
+  
+  const watchTypes = ['html', 'style', 'script', 'static']
+  for (const item of CC.tasks) {
+    if (watchTypes.includes(item.type) && item.watch) {
+      watch(getInputList(item.input), parallel(item.name)).on('change', devServerReload)
+    }
+  }
+  
+  // public 文件
+  if (publicFiles) {
+    watch(publicFiles, parallel('copy:public')).on('change', devServerReload)
+  }
+  done()
+})
+
+//== 导出任务 ==============================================
+let buildTasks = [[], [], []] // `[0: 静态资源, 1: js/css, 2: html`]
+if (publicFiles) {
+  buildTasks[0].push('copy:public')
+}
+Object.keys(taskTypes).forEach(type => {
+  const _typeTasks = taskTypes[type]
+  if (type === 'static') {
+    buildTasks[0].push(..._typeTasks.map(v => v.name))
+  }
+  if (type === 'script' || type === 'style') {
+    buildTasks[1].push(..._typeTasks.map(v => v.name))
+  }
+  if (type === 'html') {
+    buildTasks[2].push(..._typeTasks.map(v => v.name))
+  }
+})
+buildTasks = filter(v => v.length > 0) //过滤空任务
+
+exports.serve = series(
+  'del:dest',
+  parallel(...buildTasks[0]),
+  parallel(...buildTasks[1]),
+  parallel(...buildTasks[2]),
+  parallel('devServer', 'watch')
+)
+exports.build = series(
+  'del:dest',
+  parallel(...buildTasks[0]),
+  parallel(...buildTasks[1]),
+  parallel(...buildTasks[2]),
+)
