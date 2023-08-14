52643e196967998ee741713b49b8afded81eaf0a
Mervin <mengqing723@gmail.com>
2023-08-14 17:28:07 +0800

    feat(utils): Add write Js/Json file

---
+++ packages/utils/libs/jsFile.js
@@ -0,0 +1,49 @@
+const fs = require('fs-extra')
+const { stringifyJS } = require('./stringifyJS')
+
+/**
+ * 读取Json文件
+ * @param {string} file 文件路径
+ * @param {object} options 配置项
+ * @returns 
+ */
+exports.readJsonFile = async (file, options = {}) => {
+  return await fs.readJson(
+    file,
+    options
+  )
+}
+
+/**
+ * 写入Json文件
+ * @param {string} file 文件路径
+ * @param {JSON} data 数据
+ * @param {object} options 配置项
+ * @returns 
+ */
+exports.writeJsonFile = async (file, data, options = { spaces: 2 }) => {
+  return await fs.outputJson(
+    file,
+    data,
+    options
+  )
+}
+
+/**
+ * 写入Js文件
+ * @param {string} file 文件路径
+ * @param {JSON} data 数据
+ * @returns 
+ */
+exports.writeJsFile = async (file, data) => {
+  if (typeof data === 'function') {
+    data = data(stringifyJS)
+  } else {
+    data = `module.exports = ${stringifyJS(data, null, 2)}`
+  }
+
+  return await fs.outputFile(
+    file,
+    data
+  )
+}
