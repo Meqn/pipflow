b8d0cec7ea8ff99c33a17629406ad7ac2d18940b
Mervin <mengqing723@gmail.com>
2023-08-16 09:36:29 +0800

    feat(utils): add makeJSOnlyValue function

--- packages/utils/libs/stringifyJS.js
+++ packages/utils/libs/stringifyJS.js
@@ -8,3 +8,11 @@ exports.stringifyJS = function stringifyJS (value) {
     return stringify(val)
   }, 2)
 }
+
+// __expression is a special flag that allows us to customize stringification
+// output when extracting configs into standalone files
+exports.makeJSOnlyValue = function (str) {
+  const fn = () => {}
+  fn.__expression = str
+  return fn
+}
