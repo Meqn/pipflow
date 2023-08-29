e6dd5997672c1d685557462f017773deb9ba5b28
Mervin <mengqing723@gmail.com>
2023-08-29 15:09:36 +0800

    perf(core): Server-task add ui-port configuration

--- packages/core/build/serve.js
+++ packages/core/build/serve.js
@@ -10,13 +10,19 @@ module.exports = function createServeTask(name) {
   /**
    * 创建一个server服务.
    *
-   * @param {Object} options - Server服务自定义配置项 (default: {})
+   * @param {Object} option - Server服务自定义配置项 (default: {})
    * @param {Function} done - 初始化完成后的回调函数
    * @param {Object} extend - 附加选项 (default: undefined)
    * @return {Object} - Server对象
    */
-  function create(options = {}, done, extend) {
-    const serveConfig = extend === null ? options : _.merge({}, defaultConfig.server, options)
+  function create(option = {}, done, extend) {
+    const serveConfig = extend === null ? option : _.merge({}, defaultConfig.server, option)
+    if (serveConfig.port && !serveConfig.ui?.port) {
+      if (!serveConfig.ui) {
+        serveConfig.ui = {}
+      }
+      serveConfig.ui.port = parseInt(serveConfig.port, 10) + 1
+    }
     return bs.init(serveConfig, done)
   }
 
