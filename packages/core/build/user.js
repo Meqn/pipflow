4648990a4d763e56a2a5bdcf598a3cbdb9792f34
Mervin <mengqing723@gmail.com>
2023-12-03 22:37:28 +0800

    feat(core): User-task add input configuration item.

--- packages/core/build/user.js
+++ packages/core/build/user.js
@@ -1,7 +1,9 @@
+const { gulp } = require('@pipflow/utils')
 const { pipeline, onDone } = require('../base/utils')
+const { createSrcOptions } = require('./comm')
 
 module.exports = function userTask(options = {}, done) {
-  const { compiler, plugins } = options
+  const { input, compiler, plugins } = options
 
   // 1. 自定义处理函数
   if (typeof compiler === 'function') {
@@ -10,6 +12,16 @@ module.exports = function userTask(options = {}, done) {
 
   // 2. 自定义处理流程
   if (Array.isArray(plugins) && plugins.length > 0) {
+    if (input) {
+      const srcOptions = createSrcOptions(options)
+
+      return pipeline([
+        gulp.src(input, srcOptions),
+        ...plugins,
+        gulp.dest(options.dest)
+      ]).on('end', onDone(done))
+    }
+
     return pipeline(plugins).on('end', onDone(done))
   }
 
