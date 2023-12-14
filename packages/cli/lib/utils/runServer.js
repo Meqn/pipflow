12251661c4c70dd46d400451d228159f9b4b6668
Mervin <mengqing723@gmail.com>
2023-12-14 19:40:25 +0800

    perf(cli): Optimize server-command args

--- packages/cli/lib/utils/runServer.js
+++ packages/cli/lib/utils/runServer.js
@@ -6,9 +6,13 @@ module.exports = async function runTaskScript(command, args) {
   const rootDir = await pkgDir(__dirname)
   const cwd = process.cwd()
   const option = minimist(args)
-
+  
   args.unshift('run', command, '--')
-  args.push('--server=' + path.resolve(cwd, option.server || option.s || './'))
+  let serverDir = option.server || option.s
+  if (!serverDir && option._.length) {
+    serverDir = option._[0]
+  }
+  args.push('--server=' + path.resolve(cwd, serverDir || './'))
   
   return await execa('npm', args, { cwd: rootDir, stdio: 'inherit' })
 }
