36c3d8420f6a945dc8b064a7a577c8d0374afaad
Mervin <mengqing723@gmail.com>
2023-12-22 12:34:46 +0800

    perf(core): Optimize archive-task parameters

--- packages/core/build/archive.js
+++ packages/core/build/archive.js
@@ -1,13 +1,12 @@
 module.exports = function archiveTask(options = {}, done) {
   const zip = require('gulp-zip')
-  const { gulp } = require('@pipflow/utils')
+  const { gulp, timestamp } = require('@pipflow/utils')
   const { pipeline, onDone } = require('../base/utils')
   const { createSrcOptions, plumber, putProcesses } = require('./comm')
 
   const {
     input,
-    dest,
-    filename = 'archive',
+    dest
   } = options
 
   if (!input) {
@@ -23,10 +22,31 @@ module.exports = function archiveTask(options = {}, done) {
   putProcesses(processes, options.plugins)
   
   // 3. 创建zip压缩包
-  processes.push(zip(filename.endsWith('.zip') ? filename : `${filename}.zip`))
+  let filename = 'archive.zip'
+  let outDir = dest
+  // 获取压缩包名
+  const regex = /(\w+\.\w+)$/
+  if (regex.test(dest)) {
+    const destArr = dest.split('/')
+    if (destArr.length === 1) {
+      filename = destArr[0]
+      outDir = '.'
+    } else {
+      filename = destArr.pop()
+      outDir = destArr.join('/')
+    }
+  }
+  // filename 支持 [name]-[time] 和 [name]-[date].zip
+  if (filename.includes('[time]')) {
+    filename = filename.replace('[time]', timestamp('YYYYMMDDHHmmss'))
+  }
+  if (filename.includes('[date]')) {
+    filename = filename.replace('[date]', timestamp('YYYYMMDD'))
+  }
+  processes.push(zip(filename))
   
-  // 3. 输出文件
-  processes.push(gulp.dest(dest))
+  // 4. 输出文件
+  processes.push(gulp.dest(outDir))
 
   return pipeline(
     gulp.src(input, srcOptions),
