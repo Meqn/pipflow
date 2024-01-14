ed31a057ed9e45e9bae76c06d5ddc8ab7237f0d7
Mervin <mengqing723@gmail.com>
2024-01-14 19:21:44 +0800

    fix(utils): Improve deepMerge-function

--- packages/utils/libs/utils.js
+++ packages/utils/libs/utils.js
@@ -1,28 +1,35 @@
-exports.isObject = function (obj) {
+function isObject(obj) {
   return obj !== null && typeof obj === 'object'
 }
 
-exports.isPlainObject = function (obj) {
+exports.isPlainObject = function isPlainObject(obj) {
   return Object.prototype.toString.call(obj) === '[object Object]'
 }
 
 exports.deepMerge = function merge(target, ...sources) {
-  const isObject = exports.isObject
-
   if (isObject(target)) {
     for (const source of sources) {
       if (isObject(source)) {
         Object.keys(source).forEach(key => {
-          if (isObject(source[key]) && isObject(target[key])) {
-            // 递归合并对象
-            target[key] = merge(target[key], source[key])
+          if (isObject(source[key])) {
+            // 如果目标对象的相应属性也是对象，则递归合并
+            if (isObject(target[key])) {
+              target[key] = merge(target[key], source[key])
+            } else {
+              // 否则直接赋值
+              // !注: 这里是浅拷贝，某些情况下会有问题
+              target[key] = Array.isArray(source[key]) ? [...source[key]] : { ...source[key] }
+            }
           } else {
-            // 直接复制属性
+            // 非对象属性直接赋值
             target[key] = source[key]
           }
         })
       }
     }
   }
+
   return target
 }
+
+exports.isObject = isObject
