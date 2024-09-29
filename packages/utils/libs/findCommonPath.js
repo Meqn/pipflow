196334501851bf699223886f2816544e3b9cb829
mengqing <mengqing723@gmail.com>
2024-09-29 11:59:21 +0800

    fix(utils): Add `pathe` for window & refactor findCommonPath

--- packages/utils/libs/findCommonPath.js
+++ packages/utils/libs/findCommonPath.js
@@ -1,26 +1,26 @@
-const path = require('path')
+const path = require('pathe')
 
 exports.findCommonPath = function findCommonPath(paths) {
   if (!Array.isArray(paths) || paths.length === 0) {
-    return '';
+    return ''
   }
 
-  const parts = paths.map(p => {
-    const dirs = p.split(path.sep);
+  const parts = paths.map((p) => {
+    const dirs = p.split(path.sep)
     if (dirs[0] === '' || dirs[0] === '.') dirs.shift()
     return dirs
-  });
+  })
 
-  let common = '';
-  const firstPath = parts[0];
+  let common = ''
+  const firstPath = parts[0]
 
   for (let i = 0, len = firstPath.length; i < len; i++) {
-    const seg = firstPath[i];
-    if (parts.some(p => p[i] !== seg)) {
+    const seg = firstPath[i]
+    if (parts.some((p) => p[i] !== seg)) {
       break
     }
-    common = path.join(common, seg);
+    common = path.join(common, seg)
   }
 
-  return common;
+  return common
 }
