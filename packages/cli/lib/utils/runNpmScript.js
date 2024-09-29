0483d81a1d9ad9869d7ff8472ff4968034861173
mengqing <mengqing723@gmail.com>
2024-09-29 14:21:29 +0800

    refactor(cli): Imports `pathe` modules from `@pipflow/utils`

--- packages/cli/lib/utils/runNpmScript.js
+++ packages/cli/lib/utils/runNpmScript.js
@@ -1,5 +1,4 @@
-const path = require('pathe')
-const { pkgDir } = require('@pipflow/utils')
+const { pkgDir, path } = require('@pipflow/utils')
 const { execWithSign } = require('./exeCommand')
 
 // 缓存数据
@@ -9,19 +8,22 @@ module.exports = async function runTaskScript(command, args) {
   if (!rootDir) rootDir = await pkgDir(__dirname)
   if (!projectRoot) projectRoot = await pkgDir(process.cwd())
   if (!gulpFile) {
-    gulpFile = process.env.PIPFLOW_CLI_DEBUG === 'true'
-      ? path.resolve(__dirname, '../../../', 'main/tasks.js') // 本地测试环境
-      : path.resolve(projectRoot, 'node_modules/pipflow/tasks.js')
+    gulpFile =
+      process.env.PIPFLOW_CLI_DEBUG === 'true'
+        ? path.resolve(__dirname, '../../../', 'main/tasks.js') // 本地测试环境
+        : path.resolve(projectRoot, 'node_modules/pipflow/tasks.js')
   }
   //记录当前命令
   process.env.PIPFLOW_CLI_COMMAND = command
-  
+
   // 参数 `args` 依次插入 `npm [run, command, --, ...]`
   // `--gulpfile` 指定 gulpfile 文件路径
   const fullArgs = [
-    'run', command, '--',
+    'run',
+    command,
+    '--',
     `--gulpfile=${gulpFile}`,
-    `--cwd=${path.resolve(projectRoot, '.')}`
+    `--cwd=${path.resolve(projectRoot, '.')}`,
   ].concat(args)
 
   // await execa('ls', ['-la'], { cwd: projectRoot, stdio: 'inherit' })
--- packages/cli/lib/utils/runServer.js
+++ packages/cli/lib/utils/runServer.js
@@ -1,19 +1,18 @@
 module.exports = async function runTaskScript(command, args) {
-  const path = require('pathe')
-  const { pkgDir, minimist } = require('@pipflow/utils')
+  const { pkgDir, minimist, path } = require('@pipflow/utils')
   const { execWithSign } = require('./exeCommand')
 
   const rootDir = await pkgDir(__dirname)
   const cwd = process.cwd()
   const option = minimist(args)
-  
+
   args.unshift('run', command, '--')
   let serverDir = option.server || option.s
   if (!serverDir && option._.length) {
     serverDir = option._[0]
   }
   args.push('--server=' + path.resolve(cwd, serverDir || './'))
-  
+
   // return await execa('npm', args, { cwd: rootDir, stdio: 'inherit' })
   return await execWithSign('npm', args, { cwd: rootDir, stdio: 'inherit' })
 }
